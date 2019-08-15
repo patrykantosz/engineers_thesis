8c1cb89d6fe0f628eefd99875d7bdf8291e8ae80
schullstel <37815610+schullstel@users.noreply.github.com>
2019-08-15 14:04:13 +0200

    Added simple GUI for login and create account activities

--- Java/app/src/main/java/com/example/engieersthesis/MainActivity.java
+++ Java/app/src/main/java/com/example/engieersthesis/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.engieersthesis;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button logInButton;
+    private Button createAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logInButton = findViewById(R.id.logInButton);
+        createAccountButton = findViewById(R.id.createAccountButton);
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logInIntent = new Intent(MainActivity.this, LogInActivity.class);
+                startActivity(logInIntent);
+            }
+        });
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createAccountIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(createAccountIntent);
+            }
+        });
     }
+
+
 }
